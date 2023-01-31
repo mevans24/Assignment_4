84db9749d7f88ff86a3af33155e4668056da6c93
Mara <mevans4@oakland.edu>
2023-01-31 12:17:59 -0500

    First Commit

---
+++ src/A4.java
@@ -0,0 +1,7 @@
+public class A4 {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+
+        System.out.println("My name is Mara.");
+    }
+}
